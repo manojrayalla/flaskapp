370f228dd138def345299f76228929c91e5328a2
Manoj Kumar Rayalla <mrayalla1@student.gsu.edu>
2021-09-15 22:31:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,6 @@ from flask import request
 app = Flask(__name__)
 @app.route('/')
 def getagent_number():
-    server = 'manojtest.database.windows.net'
-    database = 'manojtest'
-    username = 'manojtest'
-    password = 'Manoj@143Archu'   
-    driver= '{ODBC Driver 17 for SQL Server}'
-    agentnumber = request.args.get('agentnumber')
-    conn = pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
-    cursor = conn.cursor()
-    cursor.execute("select * from dimagent where agentnum = {}".format(agentnumber))
-    check = cursor.fetchone()
-    columns = [column[0] for column in cursor.description]
-    data = dict(zip(columns, check))
-    return data
+    return "manoj"
     
     
