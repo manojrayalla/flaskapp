3f8670c04396780d74f7cef2fe33aac974f6c5a8
Manoj Kumar Rayalla <mrayalla1@student.gsu.edu>
2021-09-16 08:43:16 -0400

    Delete app_config.py

--- app_config.py
+++
@@ -1,6 +0,0 @@
-CLIENT_SECRET = "eNh7Q~bqdYGNorh66JPRO_gx-doNoJqpnzfc-"
-AUTHORITY = "https://login.microsoftonline.com/2914923e-572c-4c10-b88c-2f12bf346de7/oauth2/v2.0/token"
-CLIENT_ID = "f4c1fa71-f901-4fe7-9dd7-11434830a595"
-ENDPOINT = 'https://manojapp.azurewebsites.net/'
-SCOPE = ["User.ReadBasic.All"]
-SESSION_TYPE = "filesystem"  
