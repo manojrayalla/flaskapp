4c3bdf63bfc9157a847b6f7adb05adc3b72613c2
Manoj Kumar Rayalla <mrayalla1@student.gsu.edu>
2021-09-16 10:39:49 -0400

    Update app_config.py

--- app_config.py
+++ app_config.py
@@ -10,19 +10,7 @@ CLIENT_SECRET = "8mH7Q~MzzmSY~t8NYe_ArBJzDihhy0bs_gw3m" # Placeholder - for use 
 # if not CLIENT_SECRET:
 #     raise ValueError("Need to define CLIENT_SECRET environment variable")
 
-AUTHORITY = "https://login.microsoftonline.com/2914923e-572c-4c10-b88c-2f12bf346de7/oauth2/v2.0/token"  # For multi-tenant app
+AUTHORITY = "https://login.microsoftonline.com/2914923e-572c-4c10-b88c-2f12bf346de7"  # For multi-tenant app
 # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
-REDIRECT_PATH = "/"  # Used for forming an absolute URL to your redirect URI.
-                              # The absolute URL must match the redirect URI you set
-                              # in the app's registration in the Azure portal.
 
-# You can find more Microsoft Graph API endpoints from Graph Explorer
-# https://developer.microsoft.com/en-us/graph/graph-explorer
-ENDPOINT = 'https://testpythonwebapi.azurewebsites.net/'  # This resource requires no admin consent
-
-# You can find the proper permission names from this document
-# https://docs.microsoft.com/en-us/graph/permissions-reference
-SCOPE = ["User.ReadBasic.All"]
-
-SESSION_TYPE = "filesystem"  # Specifies the token cache should be stored in server-side session
