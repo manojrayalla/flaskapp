105212850552d8afd6006e633616b7c09edede58
Manoj Kumar Rayalla <mrayalla1@student.gsu.edu>
2021-09-16 11:26:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,27 @@ import app_config
 app = Flask(__name__)
 app.config.from_object(app_config)
 Session(app)
-@app.route('/')
+
+def getallagents():
+    server = 'manojtest.database.windows.net'
+    database = 'manojtest'
+    username = 'manojtest'
+    password = 'Manoj@143Archu'   
+    driver= '{ODBC Driver 17 for SQL Server}'
+    conn = pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = conn.cursor()
+    cursor.execute("select * from dimagent")
+    row = cursor.fetchone()
+    finaldict = dict()
+    columns = [column[0] for column in cursor.description]
+    while row:
+        list1 =[]
+        data = dict(zip(columns, row))
+        list1.append(data)
+        finaldict[str(data['agentnum'])] = list1
+        row = cursor.fetchone()
+    return finaldict
+@app.route('/selectagent')
 def getagent_number():
     server = 'manojtest.database.windows.net'
     database = 'manojtest'
@@ -22,9 +42,13 @@ def getagent_number():
     conn = pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
     cursor = conn.cursor()
     cursor.execute("select * from dimagent where agentnum = {}".format(agentnumber))
-    check = cursor.fetchone()
+    row = cursor.fetchone()
+    finaldict = dict()
     columns = [column[0] for column in cursor.description]
-    data = dict(zip(columns, check))
-    return data
-    
-    
+    while row:
+        list1 =[]
+        data = dict(zip(columns, row))
+        list1.append(data)
+        finaldict[str(data['agentnum'])] = list1
+        row = cursor.fetchone()
+    return finaldict   
